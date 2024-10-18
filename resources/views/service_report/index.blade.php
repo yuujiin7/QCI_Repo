bb3703bb5e2161c3fd79d6ba2fb2950335f59f79
Jiin <74890010+yuujiin7@users.noreply.github.com>
2024-10-18 17:19:01 +0800

    remove delete per row

--- resources/views/service_report/index.blade.php
+++ resources/views/service_report/index.blade.php
@@ -85,7 +85,7 @@ $array = array('name' => "Service Report List");; ?>
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                                             </svg>
                                         </a>
-                                        <form action="/service-report/{{ $sr->id }}" method="POST" class="inline" id="deleteFormSR{{ $sr->id }}">
+                                        <!-- <form action="/service-report/{{ $sr->id }}" method="POST" class="inline" id="deleteFormSR{{ $sr->id }}">
                                             @csrf
                                             @method('DELETE')
                                             <button class="bg-red-600 text-white px-4 py-1 rounded hover:bg-red-700 submitDelete" data-form-id="deleteFormSR{{ $sr->id }}">
@@ -93,7 +93,7 @@ $array = array('name' => "Service Report List");; ?>
                                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                                 </svg>
                                             </button>
-                                        </form>
+                                        </form> -->
                                     </div>
                                 </td>
                             </tr>
